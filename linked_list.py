5297bd2baefcbeecb55509ae4ebc252ab8b5f53e
Leonardo Araujo <leonardo.aa88@gmail.com>
2022-06-18 14:47:43 -0300

    remove duplicate node using hashtable

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Node:
     def __init__(self, value = None):
         self.value = value
@@ -34,13 +36,44 @@ class LinkedList:
                 innerNode = innerNode.next
             innerNode.next = node
 
+    def removeNode(self, node):
+        innerNode = self.head
+        previousNode = None
+        while innerNode is not None:
+            if (innerNode.value == node.value):
+                if (previousNode is None):
+                    self.head = innerNode.next
+                else:
+                    previousNode.next = innerNode.next
+                return True
+            previousNode = innerNode
+            innerNode = innerNode.next
+        return False
+
+    def removeDuplicate(self):
+        hashtable = defaultdict(int)
+        node = self.head
+        while (node is not None):
+            if (hashtable[node.value] == 1):
+                self.removeNode(node)
+                return True
+            hashtable[node.value] += 1
+            node = node.next
+        return False
+
 
 if __name__ == "__main__":
     linkedlist1 = LinkedList()
     linkedlist1.insertAtHead(Node(1))
+    linkedlist1.insertAtHead(Node(1))
     linkedlist1.insertAtHead(Node(2))
     linkedlist1.insertAtHead(Node(3))
     linkedlist1.insertAtBottom(Node(69))
+    #linkedlist1.printList()
+    #print(linkedlist1.findNode(Node(1)))
+    #print(linkedlist1.findNode(Node(42)))
+    linkedlist1.removeNode(Node(69))
+    #linkedlist1.printList()
     linkedlist1.printList()
-    print(linkedlist1.findNode(Node(1)))
-    print(linkedlist1.findNode(Node(42)))
+    linkedlist1.removeDuplicate()
+    linkedlist1.printList()
